4c7efad8398d5bb9a4ff3bcdea99bd3cee7fc963
ariel.cruz <arie.cruz@hansa.com.bo>
2026-07-04 13:09:23 -0400

    textfix: eliminar midleware

--- apps/frontend/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(req: NextRequest) {
-  console.log(req.url);
-  console.log(req);
-  return NextResponse.next();
-}
