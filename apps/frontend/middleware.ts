8a996ef717de9aaf1ca51ce7068a1b2b63f67e90
ariel.cruz <arie.cruz@hansa.com.bo>
2026-07-04 11:48:53 -0400

    fix: corrigiendo midleware

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(_request: NextRequest) {
+export function middleware(req: NextRequest) {
+  console.log(req.url);
+  console.log(req);
   return NextResponse.next();
 }
